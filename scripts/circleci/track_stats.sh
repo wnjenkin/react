738630f23aba66a5c5e5ab298ab391968ff6d79d
Whitney Jenkins <27973111+wnjenkin@users.noreply.github.com>
2018-10-05 13:15:26 -0400

    remove

--- scripts/circleci/track_stats.sh
+++ scripts/circleci/track_stats.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-set -e
-set -x
+#set -e
 
-pwd
-ls 
-ls packages
+#ALL_FILES=$(find packages -name '*.js' | grep -v umd/ | grep -v __tests__ | grep -v __mocks__)
+#COUNT_ALL_FILES=$(echo "$ALL_FILES" | wc -l)
+#COUNT_WITH_FLOW=$(grep '@flow' $ALL_FILES | perl -pe 's/:.+//' | wc -l)
+#node scripts/facts-tracker/index.js "flow-files" "$COUNT_WITH_FLOW/$COUNT_ALL_FILES"
 
-ALL_FILES=$(find packages -name '*.js' | grep -v umd/ | grep -v __tests__ | grep -v __mocks__)
-echo 1
-COUNT_ALL_FILES=$(echo "$ALL_FILES" | wc -l)
-echo 2
-COUNT_WITH_FLOW=$(grep '@flow' $ALL_FILES | perl -pe 's/:.+//' | wc -l)
-echo 3
-node scripts/facts-tracker/index.js "flow-files" "$COUNT_WITH_FLOW/$COUNT_ALL_FILES"
-echo 4
