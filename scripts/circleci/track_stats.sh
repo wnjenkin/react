dbbeda4feb129894bb5069304c0149c38649e5e9
Whitney Jenkins <27973111+wnjenkin@users.noreply.github.com>
2018-10-05 11:32:25 -0400

    Testing

--- scripts/circleci/track_stats.sh
+++ scripts/circleci/track_stats.sh
@@ -3,6 +3,9 @@
 set -e
 
 ALL_FILES=$(find packages -name '*.js' | grep -v umd/ | grep -v __tests__ | grep -v __mocks__)
+echo 1
 COUNT_ALL_FILES=$(echo "$ALL_FILES" | wc -l)
+echo 2
 COUNT_WITH_FLOW=$(grep '@flow' $ALL_FILES | perl -pe 's/:.+//' | wc -l)
+echo 3
 node scripts/facts-tracker/index.js "flow-files" "$COUNT_WITH_FLOW/$COUNT_ALL_FILES"
